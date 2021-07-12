7d10fecb3d9f8b44a8245aaebd261d2dc7ec0166
AnupLimbu <anuplimbu0527@gmail.com>
2021-07-12 19:23:32 +0545

    connect database add in a

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
     }
     $Name=$_POST["Name"];
     $Age=$_POST["Age"];
-    $Gender=$_POST["Gender"]
-    $Phone=$_POST[]
+    $Gender=$_POST["Gender"];
+    $Phone=$_POST["Phone"];
+    $Description=$_POST["Description"];
+    $ins_query="INSERT INTO `neptravel`.`customer` ( `Name`, `Age`, `Gender`, `Phone`, `Description`, `DateJoined`) VALUES ('$Name', '$Age', '$Gender', '$Phone', '$Description', current_timestamp());";
+    if ($conn->query($ins_query)==true){
+        $insert=true;
+        echo "your data has been added successfully";
+    }
+    else{
+        echo "ERROR: $ins_query <br> $conn->error";
+        
+    }
+    $conn->close();
 ?>
